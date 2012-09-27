129381b1848834f44a185d15df99566ded5d676a
oscarucb <oscarucb@5cb52b84-3326-42f4-8e96-748dc429fb88>
2012-09-27 00:20:55 +0000

    re-fix ticket 96: daily count not updating

--- SavedInstances.lua
+++ SavedInstances.lua
@@ -924,7 +924,14 @@ local function SI_GetQuestReward()
 		   ["Expires"] = expires,
 		   ["Zone"] = GetRealZoneText() }
   if isDaily then
-    t.DailyCount = GetDailyQuestsCompleted()
+    local c = 0 -- ticket 96: GetDailyQuestsCompleted() unreliable
+    for _,info in pairs(t.Quests) do
+      if info.isDaily then 
+        c = c + 1
+      end
+    end
+    debug("DailyCount: "..t.DailyCount.." => "..c)
+    t.DailyCount = c
   end
 end
 hooksecurefunc("GetQuestReward", SI_GetQuestReward)
