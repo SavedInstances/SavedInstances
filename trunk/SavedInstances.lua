ed5622a1e0abee3278516ea70f416b20d772db4d
oscarucb <oscarucb@5cb52b84-3326-42f4-8e96-748dc429fb88>
2016-10-25 08:28:20 +0000

    fix Arcway for ptBR

--- trunk/SavedInstances.lua
+++ trunk/SavedInstances.lua
@@ -887,6 +887,7 @@ addon.transInstance = {
   [531] = 161,  -- AQ temple: ticket 137 frFR
   [1228] = 897, -- Highmaul: ticket 175 ruRU
   [552] = 1011, -- Arcatraz: ticket 216 frFR
+  [1516] = 1190, -- Arcway: ticket 227/233 ptBR
 }
 
 -- some instances (like sethekk halls) are named differently by GetSavedInstanceInfo() and LFGGetDungeonInfoByID()
