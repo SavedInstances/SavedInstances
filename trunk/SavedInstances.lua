94481eacbd620b78335ddb5d45ac05c20166cc99
oscarucb <oscarucb@5cb52b84-3326-42f4-8e96-748dc429fb88>
2016-11-15 23:02:18 +0000

    add missing world boss WQ IDs

--- trunk/SavedInstances.lua
+++ trunk/SavedInstances.lua
@@ -184,11 +184,11 @@ addon.WorldBosses = {
   [1756] = { quest=42269, expansion=6, level=110 }, -- The Soultakers
   [1763] = { quest=42779, expansion=6, level=110 }, -- Shar'thos
   [1769] = { quest=43192, expansion=6, level=110 }, -- Levantus
-  --[1770] = { quest=, expansion=6, level=110 }, -- Humongris
+  [1770] = { quest=42819, expansion=6, level=110 }, -- Humongris
   [1774] = { quest=43193, expansion=6, level=110 }, -- Calamir
-  --[1783] = { quest=, expansion=6, level=110 }, -- Na'zak the Fiend
+  [1783] = { quest=43513, expansion=6, level=110 }, -- Na'zak the Fiend
   [1789] = { quest=43448, expansion=6, level=110 }, -- Drugon the Frostblood
-  --[1790] = { quest=, expansion=6, level=110 }, -- Ana-Mouz
+  [1790] = { quest=43512, expansion=6, level=110 }, -- Ana-Mouz
   [1795] = { quest=43985, expansion=6, level=110 }, -- Flotsam
   [1796] = { quest=44287, expansion=6, level=110 }, -- Withered Jim
 
