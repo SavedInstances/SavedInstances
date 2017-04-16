45d4f12d4ef48fd0c7e26b80b5a36a4018edc0d2
eTzmNcbkrng <6zopsf+367r0hfsj63go@sharklasers.com>
2017-04-17 01:39:06 +1200

    Added Currency - Brawler's Gold
    https://www.wowace.com/projects/saved_instances/issues/258
    https://archive.fo/rMPg3
    Also reorganised currencies in numerical order based on ID.

--- SavedInstances.lua
+++ SavedInstances.lua
@@ -71,36 +71,37 @@ local maxlvl = MAX_PLAYER_LEVEL_TABLE[#MAX_PLAYER_LEVEL_TABLE]
 local scantt = CreateFrame("GameTooltip", "SavedInstancesScanTooltip", UIParent, "GameTooltipTemplate")
 
 local currency = { 
-  --395, -- Justice Points 
-  1191, -- Valor Points
-  --392, -- Honor Points
-  --390, -- Conquest Points
-  738, -- Lesser Charm of Good Fortune
-  697, -- Elder Charm of Good Fortune
-  752, -- Mogu Rune of Fate
-  776, -- Warforged Seal
-  777, -- Timeless Coin
-  402, -- Ironpaw Token
-  81, -- Epicurean Award
-  515, -- Darkmoon Prize Ticket
-  241, -- Champion's Seal
-  391, -- Tol Barad Commendation
-  416, -- Mark of the World Tree
-  789, -- Bloody Coin
-  823, -- Apexis Crystal
-  824, -- Garrison Resources
-  1101,-- Oil
-  1155,-- Ancient Mana
-  1220,-- Order Resources
-  994, -- Seal of Tempered Fate
-  1129,-- Seal of Inevitable Fate
-  1166,-- Timewarped Badge 
-  1226,-- Nethershards
-  1273,-- Seal of Broken Fate
-  1149,-- Sightless Eye
-  1275,-- Curious Coin
-  1314,-- Lingering Soul Fragment
-  1342,-- Legionfall War Supplies
+	--390, -- Conquest Points
+	--392, -- Honor Points
+	--395, -- Justice Points 
+	81, -- Epicurean Award
+	241, -- Champion's Seal
+	391, -- Tol Barad Commendation
+	402, -- Ironpaw Token
+	416, -- Mark of the World Tree
+	515, -- Darkmoon Prize Ticket
+	697, -- Elder Charm of Good Fortune
+	738, -- Lesser Charm of Good Fortune
+	752, -- Mogu Rune of Fate
+	776, -- Warforged Seal
+	777, -- Timeless Coin
+	789, -- Bloody Coin
+	823, -- Apexis Crystal
+	824, -- Garrison Resources
+	994, -- Seal of Tempered Fate
+	1101,-- Oil
+	1129,-- Seal of Inevitable Fate
+	1149,-- Sightless Eye
+	1155,-- Ancient Mana
+	1166,-- Timewarped Badge 
+	1191, -- Valor Points
+	1220,-- Order Resources
+	1226,-- Nethershards
+	1273,-- Seal of Broken Fate
+	1275,-- Curious Coin
+	1299,-- Brawler's Gold
+	1314,-- Lingering Soul Fragment
+	1342,-- Legionfall War Supplies
 }
 addon.currency = currency
 
