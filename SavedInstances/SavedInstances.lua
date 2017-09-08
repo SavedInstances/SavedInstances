0af827a18effc61082db5cd4146a978a5d8d899f
Benjamin Staneck <staneck@gmail.com>
2017-09-08 05:12:45 +0200

    also iterate over the Argus map for bounties
    Closes https://github.com/eTzmNcbkrng/SavedInstances/issues/33

--- SavedInstances/SavedInstances.lua
+++ SavedInstances/SavedInstances.lua
@@ -2969,6 +2969,39 @@ function core:RefreshDailyWorldQuestInfo()
       end
     end
   end
+  local BountyQuestArgus = GetQuestBountyInfoForMapID(1184)
+  for BountyIndex, BountyInfo in ipairs(BountyQuestArgus) do
+    local title = GetQuestLogTitle(GetQuestLogIndexByID(BountyInfo.questID))
+    local timeleft = C_TaskQuest.GetQuestTimeLeftMinutes(BountyInfo.questID)
+    local _, _, isFinish, questDone, questNeed = GetQuestObjectiveInfo(BountyInfo.questID, 1, false)
+    if timeleft then
+      if timeleft > 2880 then
+        if t.DailyWorldQuest.days2 then else t.DailyWorldQuest.days2 = {} end
+        t.DailyWorldQuest.days2.name = title
+        t.DailyWorldQuest.days2.dayleft = 2
+        t.DailyWorldQuest.days2.questneed = questNeed
+        t.DailyWorldQuest.days2.questdone = questDone
+        t.DailyWorldQuest.days2.isfinish = isFinish
+        t.DailyWorldQuest.days2.iscompleted = IsQuestFlaggedCompleted(BountyInfo.questID)
+      elseif timeleft > 1440 then
+        if t.DailyWorldQuest.days1 then else t.DailyWorldQuest.days1 = {} end
+        t.DailyWorldQuest.days1.name = title
+        t.DailyWorldQuest.days1.dayleft = 1
+        t.DailyWorldQuest.days1.questneed = questNeed
+        t.DailyWorldQuest.days1.questdone = questDone
+        t.DailyWorldQuest.days1.isfinish = isFinish
+        t.DailyWorldQuest.days1.iscompleted = IsQuestFlaggedCompleted(BountyInfo.questID)
+      else
+        if t.DailyWorldQuest.days0 then else t.DailyWorldQuest.days0 = {} end
+        t.DailyWorldQuest.days0.name = title
+        t.DailyWorldQuest.days0.dayleft = 0
+        t.DailyWorldQuest.days0.questneed = questNeed
+        t.DailyWorldQuest.days0.questdone = questDone
+        t.DailyWorldQuest.days0.isfinish = isFinish
+        t.DailyWorldQuest.days0.iscompleted = IsQuestFlaggedCompleted(BountyInfo.questID)
+      end
+    end
+  end
   if IsQuestFlaggedCompleted(43341) then
     if t.DailyWorldQuest.days0 == nil then
       t.DailyWorldQuest.days0 = {}
