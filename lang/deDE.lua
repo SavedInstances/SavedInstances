4a0c2447e10edfd2e0b01665f5772b6e749880a3
gOOvER <info@goover.de>
2017-10-08 07:01:00 +0200

    Updated for using Github and wowace

---
+++ lang/deDE.lua
@@ -0,0 +1,4 @@
+﻿local L = LibStub("AceLocale-3.0"):NewLocale("SavedInstances", "deDE") 
+if not L then return end 
+
+--@localization(locale="deDE", format="lua_additive_table", handle-subnamespaces="concat")@ 
---
+++ lang/frFR.lua
@@ -0,0 +1,5 @@
+﻿local L = LibStub("AceLocale-3.0"):NewLocale("SavedInstances", "frFR")
+if not L then return end
+
+--@localization(locale="frFR", format="lua_additive_table", handle-subnamespaces="concat")@
+
