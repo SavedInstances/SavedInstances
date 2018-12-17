3feee956552af0fd6de1629f73499f1e537ddc4a
LiangYuxuan <lyx@lyx.es>
2018-12-17 19:50:17 +0800

    show both factions emissay name

--- SavedInstances/SavedInstances.lua
+++ SavedInstances/SavedInstances.lua
@@ -3721,6 +3721,13 @@ function core:ShowTooltip(anchorframe)
           if DailyInfo.name then
             if(not show[DailyInfo.dayleft] or show[DailyInfo.dayleft] == L["Emissary Missing"]) then
               show[DailyInfo.dayleft] = DailyInfo.name
+            elseif (
+              (not show[DailyInfo.dayleft]:find("/")) and
+              (show[DailyInfo.dayleft] ~= DailyInfo.name) and
+              (DailyInfo.name ~= L["Emissary Missing"])
+            ) then
+              -- shows both factions emissary name
+              show[DailyInfo.dayleft] = show[DailyInfo.dayleft] .. " / " .. DailyInfo.name
             end
             addColumns(columns, toon, tooltip)
           end
@@ -3728,16 +3735,16 @@ function core:ShowTooltip(anchorframe)
       end
     end
 
-	if not firstcategory and addon.db.Tooltip.CategorySpaces then
-          addsep()
-	end
-	if addon.db.Tooltip.ShowCategories then
-		tooltip:AddLine(YELLOWFONT .. L["Emissary Quests"] .. FONTEND)
-	end
+    if not firstcategory and addon.db.Tooltip.CategorySpaces then
+            addsep()
+    end
+    if addon.db.Tooltip.ShowCategories then
+      tooltip:AddLine(YELLOWFONT .. L["Emissary Quests"] .. FONTEND)
+    end
     for dayleft = 0 , 2 do
       if show[dayleft] then
         local showday = show[dayleft]
-			show[dayleft] = tooltip:AddLine(GOLDFONT .. showday .. " (+" .. dayleft .. " " .. L["Day"] .. ")" .. FONTEND)
+			  show[dayleft] = tooltip:AddLine(GOLDFONT .. showday .. " (+" .. dayleft .. " " .. L["Day"] .. ")" .. FONTEND)
       end
     end
     for toon, t in cpairs(addon.db.Toons, true) do
