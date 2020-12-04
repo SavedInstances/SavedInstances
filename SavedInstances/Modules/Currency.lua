8c3ef5466d705b195e6a4a6ce1bcaeeed5d44faf
LiangYuxuan <lyx@lyx.es>
2020-12-04 15:22:02 +0800

    currency: track Medallion of Service & reorder fixes #439

--- SavedInstances/Modules/Currency.lua
+++ SavedInstances/Modules/Currency.lua
@@ -67,13 +67,14 @@ local currency = {
   1754, -- Argent Commendation
   1792, -- Honor
   1822, -- Renown
-  1813, -- Reservoir Anima
-  1810, -- Redeemed Soul
-  1885, -- Grateful Offering
-  1828, -- Soul Ash
   1767, -- Stygia
+  1828, -- Soul Ash
+  1810, -- Redeemed Soul
+  1813, -- Reservoir Anima
   1816, -- Sinstone Fragments
+  1819, -- Medallion of Service
   1820, -- Infused Ruby
+  1885, -- Grateful Offering
 }
 SI.currency = currency
 
