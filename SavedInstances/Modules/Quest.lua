4bb1d0ffb1e4bae44bdd0eb671438239153f6b62
StevesExperiments <74486691+StevesExperiments@users.noreply.github.com>
2023-01-17 19:01:32 -0500

    New weekly quest.

--- SavedInstances/Modules/Quest.lua
+++ SavedInstances/Modules/Quest.lua
@@ -428,6 +428,7 @@ local QuestExceptions = {
   [70614] = "Weekly", -- Bubble Craze
   [72438] = "Weekly", -- Tarasek Intentions
   [70562] = "Weekly", -- The Plumbers, Mason
+  [66953] = "Weekly", -- All Things Fluffy
   -- Primalist Invasions
   [70723] = "Weekly", -- Shattering the Earth Primalists
   [70752] = "Weekly", -- Vaporizing the Water Primalists
